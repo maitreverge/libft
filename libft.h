c0c06025be8421b7711708ddb014a63778f31bf5
de_la_verge <verge.f@gmail.com>
2023-07-01 12:10:13 +0200

    include unistd in header file

--- libft.h
+++ libft.h
@@ -6,12 +6,14 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:56:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/06/30 18:57:45 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/01 12:09:03 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __LIBFT_H__
-#define __LIBFT_H__
+# define __LIBFT_H__
+
+#include <unistd.h>
 
 size_t	ft_strlen(const char *s);
 
