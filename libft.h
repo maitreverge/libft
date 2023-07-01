95ea947dbf5d9e2b317e05cddf5f41aa87aacdc1
de_la_verge <verge.f@gmail.com>
2023-07-01 16:49:43 +0200

    ft_strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:56:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/01 12:36:15 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/01 16:22:11 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 int		ft_isdigit(int c);
 
