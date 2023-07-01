aeb05739b567561775a010cc47c2f43bdd24d58e
de_la_verge <verge.f@gmail.com>
2023-07-01 17:06:57 +0200

    ft_isalnum

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:56:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/01 16:53:02 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/01 17:06:15 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
+int		ft_isalnum(int c);
 
 #endif
