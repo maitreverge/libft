1179f5d51b8fe1efa3141ee15a324b1c954830ad
de_la_verge <verge.f@gmail.com>
2023-07-01 12:38:41 +0200

    ft_isdigit done

--- libft.h
+++ libft.h
@@ -6,15 +6,16 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:56:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/01 12:09:03 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/01 12:36:15 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __LIBFT_H__
-# define __LIBFT_H__
+#ifndef LIBFT_H
+# define LIBFT_H
 
-#include <unistd.h>
+# include <unistd.h>
 
 size_t	ft_strlen(const char *s);
+int		ft_isdigit(int c);
 
 #endif
