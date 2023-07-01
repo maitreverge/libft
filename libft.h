be6a1bc16978646aa8824a29ae9a4e3f2b87391c
de_la_verge <verge.f@gmail.com>
2023-07-01 17:28:01 +0200

    ft_tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:56:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/01 17:18:44 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/01 17:27:06 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_tolower(int c);
 
 #endif
