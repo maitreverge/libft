11c9f7108d77c29945f127dc5a38b5abf1cb1dd2
Flow <104826504+maitreverge@users.noreply.github.com>
2023-07-03 18:26:02 +0200

    pretty libft.h

--- libft.h
+++ libft.h
@@ -16,23 +16,25 @@
 # include <unistd.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *restrict dst,
 			const char *restrict src, size_t dstsize);
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlen(const char *s);
 void	*ft_memccpy(void *restrict dst, const void *restrict src,
 			int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	ft_putchar(char c);
+void	*ft_memset(void *b, int c, size_t len);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-char	*ft_strchr(const char *s, int c);
+void	ft_bzero(void *s, size_t n);
+void	ft_putchar(char c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
@@ -40,7 +42,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
 
 #endif
