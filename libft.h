b0fd7f7c05b1482cb687236152d275e8a26ca274
de_la_verge <verge.f@gmail.com>
2023-07-03 15:22:18 +0200

    ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:56:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/03 13:33:19 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/03 15:15:42 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	ft_putchar(char c);
 void	ft_putendl_fd(char *s, int fd);
