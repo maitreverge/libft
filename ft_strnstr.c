71bd66d93bf5e3570c88ca65ab67f957c628f612
de_la_verge <verge.f@gmail.com>
2023-07-04 12:47:59 +0200

    working ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 11:44:14 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/04 12:20:44 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/04 12:46:22 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,20 @@ char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	unsigned long j;
 
 	i = 0;
-	if (ft_strlen(needle) == 0)
+	if (needle[0] == '\0')
 		return ((char *)haystack);
 	else
 	{
 		while (haystack[i] != '\0' && i < len)
 		{
 			j = 0;
-			while (haystack[i] == needle[j] && needle[j] != '\0')
+			while (haystack[i + j] == needle[j] && needle[j] != '\0' && (i + j) < len)
 			{
-				i++;
-				j++;
-				if (i == len - 1)
+				if (needle[j + 1] == '\0')
 				{
-					return (&((char *)haystack)[i]);
+					return ((char *)&haystack[i]);
 				}
+				j++;
 			}
 			i++;
 		}
