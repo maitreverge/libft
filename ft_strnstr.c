a5e0da7d585e4a5888b2253244561dc061b01217
de_la_verge <verge.f@gmail.com>
2023-07-04 12:50:01 +0200

    ft_strnstr normifix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,16 +6,16 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 11:44:14 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/04 12:46:22 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/04 12:49:13 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	unsigned long i;
-	unsigned long j;
+	unsigned long	i;
+	unsigned long	j;
 
 	i = 0;
 	if (needle[0] == '\0')
@@ -25,7 +25,8 @@ char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		while (haystack[i] != '\0' && i < len)
 		{
 			j = 0;
-			while (haystack[i + j] == needle[j] && needle[j] != '\0' && (i + j) < len)
+			while (haystack[i + j] == needle[j] && needle[j] != '\0'
+				&& (i + j) < len)
 			{
 				if (needle[j + 1] == '\0')
 				{
