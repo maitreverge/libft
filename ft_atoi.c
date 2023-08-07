4db78491884d1ce23e4dff47b72ca25107dccef0
de_la_verge <verge.f@gmail.com>
2023-08-07 10:47:30 +0200

    fast safe

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 08:58:08 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/05 10:37:54 by florianverge     ###   ########.fr       */
+/*   Updated: 2023/07/21 19:03:23 by florianverge     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,6 @@ int	is_space(char c)
 {
 	if (c == '\t' || c == '\n' || c == '\v'
 		|| c == '\f' || c == '\r' || c == ' ')
-		return (0);
-	else
-		return (1);
-}
-
-int	plus_minus(char c)
-{
-	if (c == '+' || c == '-')
 		return (0);
 	else
 		return (1);
@@ -48,21 +40,21 @@ int	ft_atoi(const char *str)
 	result = 0;
 	if (str == NULL)
 		return (0);
-	while (is_space(str[i]) == 0)
-		i++;
-	while (plus_minus(str[i]) == 0)
+	while (str[i])
 	{
+		while (is_space(str[i]) == 0)
+			i++;
 		if (str[i] == '-')
+		{
 			polarity = (polarity * -1);
-		i++;
+			i++;
+		}
+		while (is_number(str[i]) == 0)
+		{
+			result = (10 * result) + (str[i] - 48);
+			i++;
+		}
+		break ;
 	}
-	while (is_number(str[i]) == 0)
-	{
-		result = (10 * result) + (str[i] - 48);
-		i++;
-	}
-	if (result > 2147483647)
-		return (-1);
-	else
-		return (polarity * result);
+	return (polarity * result);
 }
