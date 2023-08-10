1b701ee4cbbe5553989d0028e9d03dca7c6e727f
de_la_verge <verge.f@gmail.com>
2023-08-10 12:25:43 +0200

    test change

--- ft_split.c
+++ ft_split.c
@@ -1,4 +1,4 @@
 char **ft_split(char *str)
 {
-    
+   I 
 }
--- main.c
+++ main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_atoi(const char *str);
-int main(void)
-{
-    const char* test_cases[] = {
-        "\n\n\n  -46\b9 \n5d6",     // Leading/trailing whitespace, sign, invalid character
-        printf("--------\n");
-    }
-
-    return 0;
-}
+int ft_atoi(const cha
 
 int main (void)
 {
