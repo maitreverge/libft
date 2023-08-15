4639f6303e2c90401e8f667531d9edf0f8824808
de_la_verge <verge.f@gmail.com>
2023-08-15 18:46:01 +0200

    almost working ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,73 +6,68 @@
 /*   By: florianverge <florianverge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 13:03:16 by florianverg       #+#    #+#             */
-/*   Updated: 2023/08/14 11:51:03 by florianverg      ###   ########.fr       */
+/*   Updated: 2023/08/15 18:45:47 by florianverg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int total_len(char const *str, char c)
+int count_words(char const *str, char c)
 {
-    int     i;
-    int     result;
+    int i;
+    int result;
 
     i = 0;
     result = 0;
-    while (str[i]){
-        if(str[i] == c && str[i + 1] != '\0' && str[i + 1] != c)
-            result++;
-        i++;
-    }    
+    
+    while (str[i] != '\0'){
+        while (str[i] == c && str[i + 1] == c){
+            i++;
+        }
+		if (str[i] != c){
+			result++;
+			while (str[i] != c && str[i] != '\0'){
+				i++;
+			}
+		}
+		i++;
+    }
     return (result);
 }
 
-char    *allocation(char const *s, int c, int *i)
+void	allocation(char **buffer, char const *str, char c)
 {
-    char    *mini_buffer;
-    int     j;
+	int	i; // index de s
+	int	j; // index de j
+	int	k; // index de j
 
-    j = 0;
-    while (s[*(i)] && s[*(i)] != c){
-        (*i)++;
-        j++;
-    }
-    mini_buffer = (char *)malloc(j + 1);
-    if (!mini_buffer)
-        return (NULL);
-    mini_buffer = ft_memcpy(mini_buffer, &s[*(i)], j);
-    mini_buffer[j + 1] = '\0';
-    return (mini_buffer);
+	i = 0;
+	j = 0;
+	while (str[i] != '\0'){
+		while (str[i] == c){
+            i++;
+		}
+		k = 0;
+		while (str[i] != c && str[i + 1] != '\0'){
+			i++;
+			k++;
+		}
+		buffer[j] = ft_strndup(&str[i - k], k);
+		j++;
+		i++;
+	}
 }
 
 char **ft_split(char const *s, char c)
 {
     char    **buffer;
     int     len_s;
-    int     i; // index de s
-    int     j; // index de buffer
-    
-    i = 0;
-    j = 0;
-    len_s = total_len(s, c);
-    buffer = (char **)malloc(sizeof(char *) * len_s + 1);
-    if (!buffer)
-        return (NULL);
-    while (s[i]){
-        buffer[j] = allocation(s, c, &i);
-        j++;
-    }
-    buffer[len_s + 1] = NULL;
+
+    len_s = count_words(s, c);
+	buffer = (char **)malloc(sizeof(char *) * (len_s + 1));
+	if (!buffer)
+		return (NULL);
+	allocation(buffer, s, c);
+	buffer[len_s] = NULL;
     return (buffer);
-}
-
-int main (void)
-{
-   char const *s = "Salut bande de coquines !";
-   char c = ' ';
-
-   char **result = ft_split(s, c);
-   for(int i = 0; i < 5; i++){
-    printf("Buffer[%i] = %s", i, result[i]);
-   }
 }
