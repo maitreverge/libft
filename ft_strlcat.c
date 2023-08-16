15c2043a1c96bbb869dd70addd5fb7cea1648bc3
de_la_verge <verge.f@gmail.com>
2023-08-16 18:50:13 +0200

    removed restrict word

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,21 +6,19 @@
 /*   By: florianverge <florianverge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:01:12 by florianverg       #+#    #+#             */
-/*   Updated: 2023/08/16 18:06:03 by florianverg      ###   ########.fr       */
+/*   Updated: 2023/08/16 18:49:50 by florianverg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
 	size_t	len_dest;
 	size_t	len_src;
 
-	if (dst == NULL || src == NULL)
-		return (0);
 	i = 0;
 	j = ft_strlen(dst);
 	len_dest = ft_strlen(dst);
