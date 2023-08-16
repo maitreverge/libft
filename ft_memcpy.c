b43fb9fbf506cf81436453e3b1b293c94f676f89
de_la_verge <verge.f@gmail.com>
2023-08-16 16:56:57 +0200

    secured_ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
+/*   By: florianverge <florianverge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/02 20:08:54 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/02 20:24:17 by florianverge     ###   ########.fr       */
+/*   Created: 2023/07/02 20:08:54 by florianverg       #+#    #+#             */
+/*   Updated: 2023/08/16 16:56:12 by florianverg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((char *)dst)[i] = ((char *)src)[i];
