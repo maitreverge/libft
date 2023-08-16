8c04ca3d91397f87e82ca9fc3ab214ee07c2aa50
de_la_verge <verge.f@gmail.com>
2023-08-16 20:49:15 +0200

    improved typo

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
+/*   By: florianverge <florianverge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 18:07:49 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/03 18:16:21 by florianverge     ###   ########.fr       */
+/*   Created: 2023/07/03 18:07:49 by florianverg       #+#    #+#             */
+/*   Updated: 2023/08/16 20:42:44 by florianverg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned long	i;
 
 	i = 0;
-	while ((i < n) && (s2[i] != '\0' || s1[i] != '\0'))
+	while ((i < n) && (s2[i] || s1[i]))
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
