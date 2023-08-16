a0eb86d6b312a4e48292fa8e76239c2028fc98ee
de_la_verge <verge.f@gmail.com>
2023-08-16 18:22:15 +0200

    deleted_restrict_word

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
+/*   By: florianverge <florianverge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 12:55:46 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/03 13:55:08 by florianverge     ###   ########.fr       */
+/*   Created: 2023/07/03 12:55:46 by florianverg       #+#    #+#             */
+/*   Updated: 2023/08/16 18:07:33 by florianverg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *restrict dst,
-		const void *restrict src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	char			*p_dst;
 	const char		*p_src;
