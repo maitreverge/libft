5b7b550992ba514d45e4daad34b6c05bdb851407
de_la_verge <verge.f@gmail.com>
2023-08-16 18:06:32 +0200

    secured_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florianverge <marvin@42.fr>                +#+  +:+       +#+        */
+/*   By: florianverge <florianverge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 17:01:12 by florianverge      #+#    #+#             */
-/*   Updated: 2023/07/03 17:09:21 by florianverge     ###   ########.fr       */
+/*   Created: 2023/07/03 17:01:12 by florianverg       #+#    #+#             */
+/*   Updated: 2023/08/16 18:06:03 by florianverg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	unsigned long	i;
-	unsigned long	j;
-	unsigned long	len_dest;
-	unsigned long	len_src;
+	size_t	i;
+	size_t	j;
+	size_t	len_dest;
+	size_t	len_src;
 
+	if (dst == NULL || src == NULL)
+		return (0);
 	i = 0;
 	j = ft_strlen(dst);
 	len_dest = ft_strlen(dst);
