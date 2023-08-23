0b72f3b51f216235406b46557c4a039e0d2ed721
de_la_verge <verge.f@gmail.com>
2023-08-23 20:43:03 +0200

    unfinished strtrim

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,8 +6,24 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 22:27:56 by nope              #+#    #+#             */
-/*   Updated: 2023/08/17 13:39:57 by nope             ###   ########.fr       */
+/*   Updated: 2023/08/23 19:06:41 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	size_t	i; // index s
+// 	size_t len_set;
+
+// 	if (!s1 || !set)
+// 		return (NULL);
+// 	i = 0;
+// 	len_set = ft_strlen(set);
+	
+	
+	
+// }
+
+// */
