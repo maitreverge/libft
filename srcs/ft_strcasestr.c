536277fcbb3a4948bee8e71756ad3e899329b97a
de_la_verge <verge.f@gmail.com>
2023-08-31 22:22:06 +0200

    normed_ft_strcasestr

--- srcs/ft_strcasestr.c
+++ srcs/ft_strcasestr.c
@@ -6,7 +6,7 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 13:35:26 by nope              #+#    #+#             */
-/*   Updated: 2023/08/31 19:41:25 by nope             ###   ########.fr       */
+/*   Updated: 2023/08/31 22:21:47 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*result(char *haystack_low, char *needle_low, const char *haystack)
 			if (needle_low[j + 1] == '\0')
 			{
 				free(haystack_low);
-				free(needle_low);				
+				free(needle_low);
 				return ((char *)&haystack[i]);
 			}
 			j++;
@@ -34,19 +34,15 @@ char	*result(char *haystack_low, char *needle_low, const char *haystack)
 		i++;
 	}
 	free(haystack_low);
-	free(needle_low);	
+	free(needle_low);
 	return (NULL);
 }
-
 
 char	*ft_strcasestr(const char *haystack, const char *needle)
 {
 	char	*haystack_low;
 	char	*needle_low;
-	// unsigned long	i;
-	// unsigned long	j;
 
-	// i = 0;
 	if (!haystack || !needle)
 		return (NULL);
 	if (!needle)
@@ -55,24 +51,6 @@ char	*ft_strcasestr(const char *haystack, const char *needle)
 	needle_low = ft_strdup(needle);
 	ft_strlowcase(haystack_low);
 	ft_strlowcase(needle_low);
-	// while (haystack_low[i])
-	// {
-	// 	j = 0;
-	// 	while (haystack_low[i + j] == needle_low[j] && needle_low[j])
-	// 	{
-	// 		if (needle_low[j + 1] == '\0')
-	// 		{
-	// 			free(haystack_low);
-	// 			free(needle_low);				
-	// 			return ((char *)&haystack[i]);
-	// 		}
-	// 		j++;
-	// 	}
-	// 	i++;
-	// }
-	// free(haystack_low);
-	// free(needle_low);	
-	// return (NULL);
 	return (result(haystack_low, needle_low, haystack));
 }
 
