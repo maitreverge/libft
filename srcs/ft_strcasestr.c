0cc9aa57b056dc18204cd54554b9b0a2940ba8e9
de_la_verge <verge.f@gmail.com>
2023-08-31 21:43:23 +0200

    pre_working ft_strcasestr

--- srcs/ft_strcasestr.c
+++ srcs/ft_strcasestr.c
@@ -6,28 +6,18 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 13:35:26 by nope              #+#    #+#             */
-/*   Updated: 2023/08/30 00:09:15 by nope             ###   ########.fr       */
+/*   Updated: 2023/08/31 19:41:25 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-char	*ft_strcasestr(const char *haystack, const char *needle)
+char	*result(char *haystack_low, char *needle_low, const char *haystack)
 {
-	char	*haystack_low;
-	char	*needle_low;
-	unsigned long	i;
-	unsigned long	j;
+	int	i;
+	int	j;
 
 	i = 0;
-	if (!haystack || !needle)
-		return (NULL);
-	if (!needle)
-		return ((char *)haystack);
-	haystack_low = ft_strdup(haystack);
-	needle_low = ft_strdup(needle);
-	ft_strlowcase(haystack_low);
-	ft_strlowcase(needle_low);
 	while (haystack_low[i])
 	{
 		j = 0;
@@ -46,6 +36,44 @@ char	*ft_strcasestr(const char *haystack, const char *needle)
 	free(haystack_low);
 	free(needle_low);	
 	return (NULL);
+}
+
+
+char	*ft_strcasestr(const char *haystack, const char *needle)
+{
+	char	*haystack_low;
+	char	*needle_low;
+	// unsigned long	i;
+	// unsigned long	j;
+
+	// i = 0;
+	if (!haystack || !needle)
+		return (NULL);
+	if (!needle)
+		return ((char *)haystack);
+	haystack_low = ft_strdup(haystack);
+	needle_low = ft_strdup(needle);
+	ft_strlowcase(haystack_low);
+	ft_strlowcase(needle_low);
+	// while (haystack_low[i])
+	// {
+	// 	j = 0;
+	// 	while (haystack_low[i + j] == needle_low[j] && needle_low[j])
+	// 	{
+	// 		if (needle_low[j + 1] == '\0')
+	// 		{
+	// 			free(haystack_low);
+	// 			free(needle_low);				
+	// 			return ((char *)&haystack[i]);
+	// 		}
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
+	// free(haystack_low);
+	// free(needle_low);	
+	// return (NULL);
+	return (result(haystack_low, needle_low, haystack));
 }
 
 /*
