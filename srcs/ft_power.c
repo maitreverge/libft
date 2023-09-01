95222419acf623f2350c726adfd7933174ad2b8e
de_la_verge <verge.f@gmail.com>
2023-09-01 16:37:28 +0200

    typo_norm_5 functions in one

--- srcs/ft_power.c
+++ srcs/ft_power.c
@@ -6,7 +6,7 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:15:30 by nope              #+#    #+#             */
-/*   Updated: 2023/08/20 17:58:09 by nope             ###   ########.fr       */
+/*   Updated: 2023/09/01 16:35:26 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 long	ft_power(int nb, int power)
 {
 	if (nb == 0 && power == 0)
-	  return (1);
+		return (1);
 	if (nb == 0 || power < 0)
-	  return (0);
+		return (0);
 	if (nb == 1 || power == 0)
-	  return (1);
+		return (1);
 	return (nb * ft_power(nb, power - 1));
 }
