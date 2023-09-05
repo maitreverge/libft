f373a7a7412f2bda30f5c54635878ae267eb875a
de_la_verge <verge.f@gmail.com>
2023-09-05 12:51:18 +0200

    add lst_delone

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,8 +6,16 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 13:33:07 by nope              #+#    #+#             */
-/*   Updated: 2023/09/04 13:14:05 by nope             ###   ########.fr       */
+/*   Updated: 2023/09/05 12:46:31 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst)
+		return ;
+	del(lst->content);
+	free(lst);
+}
