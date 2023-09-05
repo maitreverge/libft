b30e678b6879d79a50e15b9e6ea1445efd84396f
de_la_verge <verge.f@gmail.com>
2023-09-05 14:23:43 +0200

    improved ft_abs.c

--- ft_abs.c
+++ ft_abs.c
@@ -6,7 +6,7 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:15:17 by nope              #+#    #+#             */
-/*   Updated: 2023/09/04 13:12:23 by nope             ###   ########.fr       */
+/*   Updated: 2023/09/05 14:20:30 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int	ft_abs(int c)
 {
+	if (c > FT_INT_MAX || c <= FT_INT_MIN)
+		return (0);
 	if (c < 0)
 		return (-c);
-	return (c);
+	else
+		return (c);
 }
