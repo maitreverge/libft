d3d3e769a4441d6ebe62caf432fd1d564117a4ee
de_la_verge <verge.f@gmail.com>
2023-09-11 23:03:18 +0200

    fixed put_nbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 20:23:28 by nope              #+#    #+#             */
-/*   Updated: 2023/09/04 13:14:58 by nope             ###   ########.fr       */
+/*   Updated: 2023/09/11 23:02:35 by nope             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,16 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		ft_putchar_fd(45, fd);
-		ft_putchar_fd(50, fd);
-		ft_putnbr_fd(147483648, fd);
+		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
 	else if (n < 0)
 	{
 		ft_putchar_fd(45, fd);
-		ft_putnbr_fd((n * -1), fd);
+		n = -n;
 	}
-	else if (n > 9)
-	{
+	if (n > 9)
 		ft_putnbr_fd((n / 10), fd);
-		ft_putchar_fd(((n % 10) + 48), fd);
-	}
-	else
-		ft_putchar_fd((n + 48), fd);
+	ft_putchar_fd(((n % 10) + 48), fd);
 }
+
